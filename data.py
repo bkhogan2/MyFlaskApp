b17b8b973cd978dafaabd97e1a63bfaeeea19a52
Brian Hogan <bkhogan2@gmail.com>
2019-05-20 10:33:45 -0700

    Completed part 1 and part 2

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis assumenda eos suscipit veniam quidem repellat minus, ducimus tempore, aspernatur consequatur doloremque accusamus repellendus aliquam id magnam sit pariatur. Excepturi, doloribus!',
+            'author':'Brian Hogan',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis assumenda eos suscipit veniam quidem repellat minus, ducimus tempore, aspernatur consequatur doloremque accusamus repellendus aliquam id magnam sit pariatur. Excepturi, doloribus!',
+            'author':'John Doe',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis assumenda eos suscipit veniam quidem repellat minus, ducimus tempore, aspernatur consequatur doloremque accusamus repellendus aliquam id magnam sit pariatur. Excepturi, doloribus!',
+            'author':'Brad Traversy',
+            'create_date':'04-25-2017'
+        }
+    ]
+    return articles
